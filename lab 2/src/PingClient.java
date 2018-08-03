1ad2cc67bd66090e082f549fb885b8eb7f37b543
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-08-03 18:01:17 +1000

    Update PingClient.java

--- lab 2/src/PingClient.java
+++ lab 2/src/PingClient.java
@@ -1,6 +1,5 @@
 import java.net.*;
 import java.util.*;
-import java.io.*;
 
 public class PingClient {
     private static final int REQUEST_TIME_OUT = 1000;  // milliseconds
@@ -17,13 +16,13 @@ public class PingClient {
         // through the port specified on the command line.
         DatagramSocket port = new DatagramSocket();
         port.setSoTimeout(REQUEST_TIME_OUT);
-        //10 pings to send
+        //initalising values for end analysis
         long averageRTT = 0;
         long maximumRTT = 0;
         long minimumRTT = 5000;
         int validRTT = 0;
         for (int i = 0; i < 10; i++) {
-            //get the current time for  time-stamp
+            //get the current time for  time-stamp-start
             Date time = new Date();
             String s = time.toString();
             long timeStampStart = time.getTime();
@@ -31,7 +30,6 @@ public class PingClient {
             String message = "PING " + i + " " + s + "\r\n";
             //this is the packet used in the ping request containing the message.
             DatagramPacket pingRequest = new DatagramPacket(message.getBytes(), message.length(), server, Integer.parseInt(args[1]));
-
             //now we want to send the ping request to the port
             port.send(pingRequest);
             //now we want to receive the response from the server.
@@ -40,26 +38,33 @@ public class PingClient {
             try {
                 //if there is a response from the server within 1s since timeout at 1s will trigger the exception
                 port.receive(serverReply);
+                //use the current time after message received for time-stamp-end
                 Date time2 = new Date();
                 //calculate the RTT from this
                 long timeStampEnd = time2.getTime();
+                //the round trip time is the final time subtracted from the initial time
                 long RTT = timeStampEnd - timeStampStart;
+                //display the ping message
                 System.out.println("ping to " + args[0] + ", seq = " + i + ", rtt = " + RTT + " ms");
                 averageRTT += RTT;
                 validRTT++;
+                //if the RTT from this iteration is larger than the maximum update maximum
                 if (RTT > maximumRTT)
                     maximumRTT = RTT;
+                //if the RTT from this iteration is smaller than the minimum update minimum
                 if (RTT < minimumRTT)
                     minimumRTT = RTT;
-                Thread.sleep(1000-RTT);
+                //program waits for 1 second - the amount of time for the RTT to simulate 1 second delay
+                Thread.sleep(1000 - RTT);
             } catch (Exception e) {
+                //time out exception
                 System.out.println("ping to " + args[0] + ", seq = " + i + ", rtt = time out");
+                //1 second delay between the ping
                 Thread.sleep(1000);
             }
         }
         //print the average minimum and maximum RTT
-        System.out.println("Average RTT = " + (averageRTT/validRTT) + " ms" + ", Minimum RTT = "
+        System.out.println("Average RTT = " + (averageRTT / validRTT) + " ms" + ", Minimum RTT = "
                 + minimumRTT + " ms" + ", Maximum RTT = " + maximumRTT + " ms.");
     }
 }
-
