7cb22e8f89a39c7cb297b9e5a33e349660a35aea
Abanob Tawfik <z5075490@ad.unsw.edu.au>
2018-08-17 12:35:45 +1000

    beginning project

---
+++ STP/src/Receiver.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class Receiver {
+    public static void main(String args[]){
+        if(args.length != 2){
+            System.out.println("usage: ");
+        }
+    }
+}
