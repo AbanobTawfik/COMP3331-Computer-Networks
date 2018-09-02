f8a176fecb9029e493d41fce53ce177f32706aeb
Abanob Tawfik <z5075490@ad.unsw.edu.au>
2018-09-03 05:31:57 +1000

    need to implement the 3 way handshake now

--- STP/src/Receiver.java
+++ STP/src/Receiver.java
@@ -1,11 +1,14 @@
-import java.util.*;
-import java.io.*;
-import java.net.*;
-
 public class Receiver {
     public static void main(String args[]){
         if(args.length != 2){
             System.out.println("usage: ");
         }
+        Receiver receiver = new Receiver();
+        receiver.operate(args);
+    }
+
+    public void operate(String args[]){
+        STPReceiver receiver = new STPReceiver(args);
+        receiver.operate();
     }
 }
--- STP/src/STPReceiver.java
+++ STP/src/STPReceiver.java
@@ -1,2 +1,26 @@
+import java.net.*;
+
 public class STPReceiver {
+    private InetAddress IP;
+    private int portNumber;
+    private DatagramSocket socket;
+    private String fileRequested;
+
+    public STPReceiver(String args[]){
+        this.portNumber = Integer.parseInt(args[0]);
+        this.fileRequested = args[1];
+        try{
+            this.socket = new DatagramSocket();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void operate(){
+
+    }
+
+    public void handshake(){
+
+    }
 }
