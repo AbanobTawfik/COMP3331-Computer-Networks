61865f60d439494f8dc300dd171684c1e12182bf
Abanob Tawfik <z5075490@ad.unsw.edu.au>
2018-10-02 20:20:09 +1000

    will multi-thread send and receive

--- STP/src/STPSender.java
+++ STP/src/STPSender.java
@@ -184,6 +184,7 @@ public class STPSender {
                     socket.receive(dataIn);
                     r = new ReadablePacket(dataIn);
                     if (r.isACK()) {
+                        //
                         for (ReadablePacket read : window) {
                             if (r.getAcknowledgemntNumber() == read.getSequenceNumber()) {
                                 window.remove(read);
