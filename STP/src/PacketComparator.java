22202fdf1e4d944a9df6a1fb56a243787b73df86
Abanob Tawfik <z5075490@ad.unsw.edu.au>
2018-10-09 17:35:09 +1100

    commenting my code and javadoc slowly very slowly getting it done

--- STP/src/PacketComparator.java
+++ STP/src/PacketComparator.java
@@ -1,8 +1,10 @@
 import java.util.Comparator;
 
+/**
+ * This class will be used as a comparator for our priority queue. it will
+ * compare packets based on sequence numbers in ascending order
+ */
 public class PacketComparator implements Comparator<ReadablePacket> {
-
-
     @Override
     public int compare(ReadablePacket p1, ReadablePacket p2) {
         if (p1.getSequenceNumber() < p2.getSequenceNumber())
--- STP/src/STPTimer.java
+++ STP/src/STPTimer.java
@@ -1,15 +1,22 @@
+/**
+ * This class will be a counting timer running on a seperate thread
+ * it will run on both sender and receiver calculating time since the start of execution
+ */
 public class STPTimer extends Thread{
+    //initialise the start time since run
     private long timeStarted = System.currentTimeMillis();
+    //to calculate time passed we want to use this value to store
+    //current time - time since started
     private long millisecondsElapsed = 0;
-
+    //run the timer on a seperate thread
     @Override
     public void run() {
         new Thread(() -> {
-            //infinitely generating boards with a delay
             while (true) {
+                //calculate the difference between start time and current time
                 millisecondsElapsed = System.currentTimeMillis() - timeStarted;
-                //System.out.println("time elapsed - " + millisecondsElapsed/1000);
                 try{
+                    //sleep for 100ms before next computation
                     Thread.sleep(100);
                 }catch(Exception e){
                     e.printStackTrace();
@@ -18,12 +25,24 @@ public class STPTimer extends Thread{
         }).start();
     }
 
-    public long timePassed(){
+    /**
+     * This method will return the time since the start of execution
+     *
+     * @return the time since start of execution
+     */
+    public long timePassed()
+    {
+        //return the time elapsed since start of program
         return millisecondsElapsed;
     }
 
+    /**
+     * reset the timer to reset time elapsed
+     */
     private void reset(){
+        //reset the milliseconds elapsed
         this.millisecondsElapsed = 0;
+        //set the start time to be the current system time
         timeStarted = System.currentTimeMillis();
     }
 }
