de9e41f4e757b31ecd52e3e11c64b667bc947b3e
Abanob Tawfik <z5075490@ad.unsw.edu.au>
2018-10-09 15:33:21 +1100

    commenting my code and javadoc slowly very slowly getting it done

--- STP/src/PacketSet.java
+++ STP/src/PacketSet.java
@@ -1,79 +1,147 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 
+/**
+ * This class will be used as a set to store packets received from the sender. the class will maintain
+ * order within sequence number of the packet. the class will also not add any duplicate packets to the set
+ * an array list is used to maintain the state of our set
+ */
 public class PacketSet {
+    //the arraylist that will store the packets in the set
     private ArrayList<ReadablePacket> set = new ArrayList<ReadablePacket>();
 
-    public void add(ReadablePacket r){
-        if(r == null)
+    /**
+     * this method will add a packet to the set based on packet details.
+     * first we check if the packet exists in the set, if it does we return
+     * if the packet is null, we return. otherwise we will add it to the arraylist
+     * and then sort the array list
+     *
+     * @param r the packet we are attempting to add to the set
+     */
+    public void add(ReadablePacket r) {
+        //if the packet is null, return
+        if (r == null)
             return;
-        for(ReadablePacket r1: set){
-            if(r.getSequenceNumber() == r1.getSequenceNumber())
+        //scan through the current set
+        for (ReadablePacket r1 : set) {
+            //if the packet exists in the set, return
+            if (r.getSequenceNumber() == r1.getSequenceNumber())
                 return;
         }
-            set.add(r);
+        //otherwise add the packet to the set
+        set.add(r);
+        //sort the set
         sortSet();
     }
 
-    public void remove(ReadablePacket r){
-        if(!set.contains(r))
+    /**
+     * this method will remove a packet from the set provided the packet exists within the set.
+     *
+     * @param r the packet we are attempting to remove from the set
+     */
+    public void remove(ReadablePacket r) {
+        //if the packet doesnt exist within our set, return
+        if (!set.contains(r))
             return;
+        //otherwise we remove the packet from the set, and sort the set to maintain order
         set.remove(r);
         sortSet();
 
     }
 
-    public ReadablePacket get(int index){
-        if(index < 0 || index >= set.size())
+    /**
+     * Get readable packet.
+     *
+     * @param index the index
+     * @return the readable packet
+     */
+    public ReadablePacket get(int index) {
+        if (index < 0 || index >= set.size())
             return null;
         return set.get(index);
     }
 
-    public int last(){
-        if(set.size() == 0)
+    /**
+     * Last int.
+     *
+     * @return the int
+     */
+    public int last() {
+        if (set.size() == 0)
             return 0;
-        if(set.size() == 1)
+        if (set.size() == 1)
             return set.get(0).getSequenceNumber();
-        return set.get(set.size()-1).getSequenceNumber();
+        return set.get(set.size() - 1).getSequenceNumber();
     }
 
-    public ReadablePacket get(ReadablePacket r){
-        if(!set.contains(r))
+    /**
+     * Get readable packet.
+     *
+     * @param r the r
+     * @return the readable packet
+     */
+    public ReadablePacket get(ReadablePacket r) {
+        if (!set.contains(r))
             return null;
-        if(set.indexOf(r) == -1)
+        if (set.indexOf(r) == -1)
             return null;
         return set.get(set.indexOf(r));
     }
 
-    public int size(){
+    /**
+     * Size int.
+     *
+     * @return the int
+     */
+    public int size() {
         return set.size();
     }
 
-    public int indexOf(ReadablePacket r){
+    /**
+     * Index of int.
+     *
+     * @param r the r
+     * @return the int
+     */
+    public int indexOf(ReadablePacket r) {
         return set.indexOf(r);
     }
 
 
-    public ArrayList<ReadablePacket> getArrayList(){
+    /**
+     * Get array list array list.
+     *
+     * @return the array list
+     */
+    public ArrayList<ReadablePacket> getArrayList() {
         return set;
     }
 
-    public boolean contains(ReadablePacket r){
-        for(ReadablePacket r1:set){
-            if(r1.getSequenceNumber() == r.getSequenceNumber())
+    /**
+     * Contains boolean.
+     *
+     * @param r the r
+     * @return the boolean
+     */
+    public boolean contains(ReadablePacket r) {
+        for (ReadablePacket r1 : set) {
+            if (r1.getSequenceNumber() == r.getSequenceNumber())
                 return true;
         }
         return false;
     }
 
-    public void sortSet(){
-        set.sort(new Comparator<ReadablePacket>(){
+    /**
+     * Sort set.
+     */
+    public void sortSet() {
+        set.sort(new Comparator<ReadablePacket>() {
 
             @Override
             public int compare(ReadablePacket o1, ReadablePacket o2) {
-                if(o1.getSequenceNumber() < o2.getSequenceNumber())
+                if (o1.getSequenceNumber() < o2.getSequenceNumber())
                     return -1;
-                if(o1.getSequenceNumber() > o2.getSequenceNumber())
+                if (o1.getSequenceNumber() > o2.getSequenceNumber())
                     return 1;
                 return 0;
             }
--- STP/src/Unreliability.java
+++ STP/src/Unreliability.java
@@ -1,4 +1,10 @@
+/**
+ * This class will be used to store PLD module data
+ * to avoid over-crowding sender fields. it will also
+ * contain the counter for statistics to print on the log files
+ */
 public class Unreliability {
+    //these are the unreliability factors passed from program arguements in Sender
     private float pDrop;
     private float pDuplicate;
     private float pCorrupt;
@@ -7,6 +13,7 @@ public class Unreliability {
     private float pDelay;
     private int maxDelay;
     private long seed;
+    //counters for statistics inside our log files
     private int packetsDropped = 0;
     private int packetsDuplicated = 0;
     private int packetsReordered = 0;
@@ -18,7 +25,21 @@ public class Unreliability {
     private int packetsTimedOut = 0;
     private int duplicateACKS = 0;
 
-    public Unreliability(float pDrop, float pDuplicate, float pCorrupt, float pOrder, int maxOrder, float pDelay, int maxDelay, long seed) {
+    /**
+     * This method will be used to store all PLD error variables into a single class for simplicity sakes
+     *
+     * @param pDrop      the probability to drop a packet
+     * @param pDuplicate the probability to duplicate a packet
+     * @param pCorrupt   the probability to corrupt a packet
+     * @param pOrder     the probability to re-order a packet
+     * @param maxOrder   the maximum number of packets we send before sending our packet we held onto
+     * @param pDelay     the probability to delay a packet
+     * @param maxDelay   the maximum delay that occurs due to probability of delayed pakcets
+     * @param seed       the seed for our random number generator
+     */
+    public Unreliability(float pDrop, float pDuplicate, float pCorrupt, float pOrder,
+                         int maxOrder, float pDelay, int maxDelay, long seed) {
+        //set all PLD error values directly from program input
         this.pDrop = pDrop;
         this.pDuplicate = pDuplicate;
         this.pCorrupt = pCorrupt;
@@ -29,116 +50,264 @@ public class Unreliability {
         this.seed = seed;
     }
 
+    /**
+     * This method will be used to get the probability to drop a packet directly from the PLD module
+     *
+     * @return the probability to drop a packet
+     */
     public float getpDrop() {
+        //return the probability to drop a packet
         return pDrop;
     }
 
+    /**
+     * This method will be used to get the probability to duplicate a packet directly from the PLD module
+     *
+     * @return the probability to duplicate a packet
+     */
     public float getpDuplicate() {
+        //return the probability to duplicate a packet
         return pDuplicate;
     }
 
+    /**
+     * This method will be used to get the probability to corrupt a packet directly from the PLD module
+     *
+     * @return the probability to corrupt a packet
+     */
     public float getpCorrupt() {
+        //return the probability to corrupt a packet
         return pCorrupt;
     }
 
+    /**
+     * This method will be used to get the probability to re-order a packet directly from the PLD module
+     *
+     * @return the probability to re-order a packet
+     */
     public float getpOrder() {
+        //return the probability to re-order a packet
         return pOrder;
     }
 
+    /**
+     * This method will be used to get the maximum number of packets we send before we send our re-ordered packet
+     *
+     * @return the maximum packets in re-order
+     */
     public int getMaxOrder() {
+        //return the maximum packets in re-order
         return maxOrder;
     }
 
+    /**
+     * This method will be used to get the probability to delay a packet directly from the PLD module
+     *
+     * @return the probability to delay a packet
+     */
     public float getpDelay() {
+        //return the probability to delay a packet
         return pDelay;
     }
 
+    /**
+     * This method will be used to get the maximum delay used in pOrder, will be the upperbounds of the delay used
+     *
+     * @return maximum upperbounds of the delay used in pDelay
+     */
     public int getMaxDelay() {
+        //return the maximum amount of delay used in pDelay
         return maxDelay;
     }
 
+    /**
+     * This method returns the seed used in our random number generator
+     *
+     * @return the seed used in the random number generator
+     */
     public long getSeed() {
+        //return the seed used in our random number generator
         return seed;
     }
 
 
+    /**
+     * This method returns the number of packets dropped by the sender in the PLD module
+     *
+     * @return the number of packets dropped by the sender
+     */
     public int getPacketsDropped() {
+        //return the amount of packets dropped by the sender
         return packetsDropped;
     }
 
+    /**
+     * This method returns the number of packets duplicated by the sender in the PLD module
+     *
+     * @return the number of packets duplicated by the sender
+     */
     public int getPacketsDuplicated() {
+        //return the number of packets duplicated by the sender
         return packetsDuplicated;
     }
 
+    /**
+     * This method returns the number of packets reordered by the sender in the PLD module
+     *
+     * @return the number of packets reordered by the sender
+     */
     public int getPacketsReordered() {
+        //return the number of packets reordered by the sender
         return packetsReordered;
     }
 
+    /**
+     * This method returns the number of packets delayed by the sender in the PLD module
+     *
+     * @return the number of packets delayed by the sender
+     */
     public int getPacketsDelayed() {
+        //return the number of packets delayed by the sender
         return packetsDelayed;
     }
 
+    /**
+     * This method returns the number of packets corrupted by the sender in the PLD module
+     *
+     * @return the number of packets corrupted by the sender
+     */
     public int getPacketsCorrupted() {
+        //return the number of packets corrupted by the sender
         return packetsCorrupted;
     }
 
+    /**
+     * This method returns the number of fast re-transmissions occured
+     *
+     * @return the number of fast re-transmissions occured
+     */
     public int getFastRetransmissions() {
+        //return the number of fast re-transmissions occured
         return fastRetransmissions;
     }
 
+    /**
+     * This method will return the number of packets handled by the PLD
+     *
+     * @return the number of packets handled by the PLD
+     */
     public int getPLDTransferredPackets() {
+        //return the number of packets handled by the PLD
         return PLDTransferredPackets;
     }
 
+    /**
+     * This method will return the number of packets transferred by the sender
+     *
+     * @return the number of packets transferred by the sender
+     */
     public int getPacketsTransferred() {
+        //return the number of packets transferred by the sender
         return packetsTransferred;
     }
 
+    /**
+     * This method will return the number of packets that timed out
+     *
+     * @return the number of packets that timed out
+     */
     public int getPacketsTimedOut() {
+        //return the number of packets that timed out
         return packetsTimedOut;
     }
 
+    /**
+     * This method will return the number of duplicate acks received from the receiver
+     *
+     * @return the number of duplicate acks received from the receiver
+     */
     public int getDuplicateACKS() {
+        //return the number of duplicate acks received from the receiver
         return duplicateACKS;
     }
 
-    public void addPLDTransferred(){
+    /**
+     * This method when called will increment the counter for packets throught he PLD by 1
+     */
+    public void addPLDTransferred() {
+        //increment the pld transfer packet counter by 1
         PLDTransferredPackets++;
     }
 
-    public void addPacketsTransferred(){
+    /**
+     * This method when called will increment the counter for packets transferred by 1
+     */
+    public void addPacketsTransferred() {
+        //increment the transfer packet counter by 1
         packetsTransferred++;
     }
 
+    /**
+     * This method when called will increment the counter for packets dropped by 1
+     */
     public void addPacketDropped() {
+        //increment the packets dropped counter by 1
         packetsDropped++;
     }
 
+    /**
+     * This method when called will increment the counter for packets delayed by 1
+     */
     public void addPacketDelayed() {
+        //increment the packets delayed counter by 1
         packetsDelayed++;
     }
 
+    /**
+     * This method when called will increment the counter for packets corrupted by 1
+     */
     public void addPacketCorrupted() {
+        //increment the packets delayed counter by 1
         packetsCorrupted++;
     }
 
+    /**
+     * This method when called will increment the counter for packets reordered by 1
+     */
     public void addPacketReOrdered() {
+        //increment the packets reordered counter by 1
         packetsReordered++;
     }
 
+    /**
+     * This method when called will increment the counter for packets duplicated by 1
+     */
     public void addPacketDuplicated() {
+        //increment the packets duplicated counter by 1
         packetsDuplicated++;
     }
 
+    /**
+     * This method when called will increment the counter for fast retransmissions by 1
+     */
     public void addFastRetransmissions() {
+        //increment the fast retransmissions counter by 1
         fastRetransmissions++;
     }
 
-    public void addPacketsTimedOut(){
+    /**
+     * This method when called will increment the counter for number of timeouts by 1
+     */
+    public void addPacketsTimedOut() {
+        //increment the number of timeouts counter by 1
         packetsTimedOut++;
     }
 
-    public void addDuplicateACKS(){
+    /**
+     * This method when called will increment the counter for number of duplicate acks received by 1
+     */
+    public void addDuplicateACKS() {
+        //increment the number of duplicate acks received counter by 1
         duplicateACKS++;
     }
 }
